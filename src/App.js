30634e645edc2c935671fe31d68ab461b49740db
Joyce Lau <jshizuki@yahoo.com.hk>
2024-01-29 21:06:59 +0900

    Added header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import SearchBar from "./components/SearchBar";
 // CSS styles
-// import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <SearchBar />
-      {/* Add other components here */}
-    </ThemeProvider>
+    <div className={styles.appContainer}>
+      <ThemeProvider theme={theme}>
+        <div className={styles.bg}>
+          <p className={styles.header}>
+            Ja<span style={{ color: "#ff0000" }}>mmm</span>ing
+          </p>
+          <SearchBar />
+        </div>
+
+        {/* Add other components here */}
+      </ThemeProvider>
+    </div>
   );
 }
 
